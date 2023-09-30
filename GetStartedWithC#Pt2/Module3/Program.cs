41125cd9bcd23266b4ead33d7be50a061ddca3f7
cgratts <camgratts@yahoo.com>
2023-09-30 15:31:36 -0500

    Microsoft - Get started with C#, Part 2
    Create and run simple C# console applications

---
+++ GetStartedWithC#Pt2/Module3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Module 3: Store and iterate through sequences of data using Arrays and the foreach statement in C#
+
+// 1. Exercise - Get started with array basics
+/*
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" }; // Initialize and assign values to elements of an array
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}"); // Retrieve values from elements of an array
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000"; // Reassign the value of an array
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process."); // Use the Length property of an array
+*/
+
+// 2. Exercise - Implement the foreach statement
+/*
+int[] inventory = { 200, 450, 700, 175, 250 }; // Create and initialize an array of int
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory) // Add a foreach statement to iterate through the array
+{
+    sum += items; // Sum the value of each element in the array
+    bin++; // Hold the current bin number and display the running total
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
+*/
+
+// 3. Exercise - Complete a challenge activity for nested iteration and selection statements
+/*
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs) // Iterate through array and output order IDs that begin with the letter B
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
+*/
---
+++ GetStartedWithC#Pt2/Module4/Program.cs
@@ -0,0 +1,92 @@
+﻿// Module 4: Create readable code with conventions, whitespace, and comments in C#
+
+// 1. Choose variable names that follow rules and conventions
+/*
+char userOption;
+int gameScore;
+float particlesPerMillion;
+bool processedCustomer;
+*/
+
+// 2. Exercise - Create effective code comments
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+/*
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+*/
+
+// 3. Exercise - Use whitespace to make your code easier to read
+/*
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } 
+    else 
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+*/
+
+// 4. Exercise - Complete a challenge activity to improve code readability 
+
+// Apply style guidelines to improve readability
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+/*
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray(); // Convert string to char array
+Array.Reverse(message); // Reverse array of chars
+
+int letterCount = 0;
+
+foreach (char letter in message) // Count number of o's in char array
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message); // Convert char array to string
+
+Console.WriteLine(newMessage); 
+Console.WriteLine($"'o' appears {letterCount} times."); // Display count
+*/
